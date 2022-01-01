01d94f8d39b69af77907ac44c46fdac59bd356fd
napuh <naamanynadiemas@gmail.com>
2022-01-01 19:06:50 +0100

    Actualizados los att de producto para que encajen con el nuevo planteamiento de la BBDD

--- src/modelo/VO/ProductoVO.java
+++ src/modelo/VO/ProductoVO.java
@@ -8,13 +8,27 @@ public class ProductoVO {
     Blob imagen;
     String material;
     ProveedorVO proveedor;
+    int IDVenta;
 
+    public ProductoVO(String tipoPieza, double precio, Blob imagen, String material, ProveedorVO proveedor,
+            int IDVenta) {
+        this.tipoPieza = tipoPieza;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.material = material;
+        this.proveedor = proveedor;
+        this.IDVenta = IDVenta;
+    }
+
+    // constructor para crear un producto sin venta. Si el ID de la venta es 0, el
+    // producto NO esta vendido
     public ProductoVO(String tipoPieza, double precio, Blob imagen, String material, ProveedorVO proveedor) {
         this.tipoPieza = tipoPieza;
         this.precio = precio;
         this.imagen = imagen;
         this.material = material;
         this.proveedor = proveedor;
+        this.IDVenta = 0;
     }
 
     public String getTipoPieza() {
