fb633e12c75f2dc89963d520a62282dce6351d95
napuh <naamanynadiemas@gmail.com>
2022-01-01 19:05:36 +0100

    Algo habre formateado no me acuerdo

--- src/Vista/Vista/VistaGeneral.java
+++ src/Vista/Vista/VistaGeneral.java
@@ -48,32 +48,32 @@ public class VistaGeneral {
 		frame.setBackground(new Color(105, 105, 105));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setForeground(new Color(255, 255, 255));
 		menuBar.setBackground(new Color(128, 128, 128));
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu UsuarioMenuItem = new JMenu("Usuario");
 		UsuarioMenuItem.setBackground(new Color(128, 128, 128));
 		UsuarioMenuItem.setForeground(new Color(255, 255, 255));
 		menuBar.add(UsuarioMenuItem);
-		
+
 		JMenuItem aniadirUsuario = new JMenuItem("Aniadir");
 		aniadirUsuario.setBackground(new Color(128, 128, 128));
 		aniadirUsuario.setForeground(new Color(255, 255, 255));
 		UsuarioMenuItem.add(aniadirUsuario);
-		
+
 		JMenuItem modificarUsuario = new JMenuItem("Modificar");
 		modificarUsuario.setForeground(new Color(255, 255, 255));
 		modificarUsuario.setBackground(new Color(128, 128, 128));
 		UsuarioMenuItem.add(modificarUsuario);
-		
+
 		JMenuItem eliminarUsuario = new JMenuItem("Eliminar");
 		eliminarUsuario.setBackground(new Color(128, 128, 128));
 		eliminarUsuario.setForeground(new Color(255, 255, 255));
 		UsuarioMenuItem.add(eliminarUsuario);
-		
+
 		JMenu ProveedorMenuItem = new JMenu("Proveedor");
 		ProveedorMenuItem.setBackground(new Color(128, 128, 128));
 		ProveedorMenuItem.setForeground(new Color(255, 255, 255));
@@ -83,63 +83,63 @@ public class VistaGeneral {
 		aniadirProveedor.setForeground(new Color(255, 255, 255));
 		aniadirProveedor.setBackground(new Color(128, 128, 128));
 		ProveedorMenuItem.add(aniadirProveedor);
-		
+
 		JMenuItem modificarProveedor = new JMenuItem("Modificar");
 		modificarProveedor.setForeground(new Color(255, 255, 255));
 		modificarProveedor.setBackground(new Color(128, 128, 128));
 		ProveedorMenuItem.add(modificarProveedor);
-		
+
 		JMenuItem eliminarProveedor = new JMenuItem("Eliminar");
 		eliminarProveedor.setForeground(new Color(255, 255, 255));
 		eliminarProveedor.setBackground(new Color(128, 128, 128));
 		ProveedorMenuItem.add(eliminarProveedor);
-		
+
 		JMenu ProductoMenuItem = new JMenu("Producto");
 		ProductoMenuItem.setBackground(new Color(128, 128, 128));
 		ProductoMenuItem.setForeground(new Color(255, 255, 255));
 		menuBar.add(ProductoMenuItem);
-		
+
 		JMenuItem aniadirProducto = new JMenuItem("Aniadir");
 		aniadirProducto.setForeground(new Color(255, 255, 255));
 		aniadirProducto.setBackground(new Color(128, 128, 128));
 		ProductoMenuItem.add(aniadirProducto);
-		
+
 		JMenuItem modificarProducto = new JMenuItem("Modificar");
 		modificarProducto.setForeground(new Color(255, 255, 255));
 		modificarProducto.setBackground(new Color(128, 128, 128));
 		ProductoMenuItem.add(modificarProducto);
-		
+
 		JMenuItem eliminarProducto = new JMenuItem("Eliminar");
 		eliminarProducto.setForeground(new Color(255, 255, 255));
 		eliminarProducto.setBackground(new Color(128, 128, 128));
 		ProductoMenuItem.add(eliminarProducto);
-		
+
 		JMenu FacturaMenuItem = new JMenu("Facturas");
 		FacturaMenuItem.setBackground(new Color(128, 128, 128));
 		FacturaMenuItem.setForeground(new Color(255, 255, 255));
 		menuBar.add(FacturaMenuItem);
-		
+
 		JMenu BackupMenuItem = new JMenu("Backup");
 		BackupMenuItem.setBackground(new Color(128, 128, 128));
 		BackupMenuItem.setForeground(new Color(255, 255, 255));
 		menuBar.add(BackupMenuItem);
-		
+
 		JMenuItem crearBackup = new JMenuItem("Crear");
 		crearBackup.setForeground(new Color(255, 255, 255));
 		crearBackup.setBackground(new Color(128, 128, 128));
 		BackupMenuItem.add(crearBackup);
-		
+
 		JMenuItem restaurarBackup = new JMenuItem("Restaurar");
 		restaurarBackup.setForeground(new Color(255, 255, 255));
 		restaurarBackup.setBackground(new Color(128, 128, 128));
 		BackupMenuItem.add(restaurarBackup);
-		
+
 		JMenu AyudaMenuItem = new JMenu("Ayuda");
 		AyudaMenuItem.setBackground(new Color(128, 128, 128));
 		AyudaMenuItem.setForeground(new Color(255, 255, 255));
 		menuBar.add(AyudaMenuItem);
 		frame.getContentPane().setLayout(new GridLayout(1, 2, 0, 0));
-		
+
 		JButton botonVenta = new JButton("Venta");
 		botonVenta.setForeground(new Color(255, 255, 255));
 		botonVenta.setBackground(new Color(50, 205, 50));
@@ -149,7 +149,7 @@ public class VistaGeneral {
 			}
 		});
 		frame.getContentPane().add(botonVenta);
-		
+
 		JButton botonCatalogo = new JButton("Catalogo");
 		botonCatalogo.setForeground(new Color(255, 255, 255));
 		botonCatalogo.setBackground(new Color(102, 205, 170));
