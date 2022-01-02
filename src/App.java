b3084d1fd1ff7d68aba716bc70c4706e45ee332d
napuh <naamanynadiemas@gmail.com>
2022-01-02 14:17:49 +0100

    quitada mierda del main

--- src/App.java
+++ src/App.java
@@ -1,24 +1,6 @@
-import globals.enums.TipoUsuario;
-import modelo.DAO.UsuarioDAO;
-import modelo.VO.UsuarioVO;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        UsuarioVO napuh = new UsuarioVO("123456F", "Naaman", "nhuerp00", "root", TipoUsuario.CAJERO);
-        UsuarioDAO udao = new UsuarioDAO();
-        if (udao.create(napuh)) {
-            System.out.println("se ha creado");
-        } else {
-            System.out.println("No se ha creado");
-        }
-
-        UsuarioVO copiaNap = (UsuarioVO) udao.search(napuh);
-        System.out.println("DNI DE COPIANAP: " + copiaNap.getDNI());
-        System.out.println("==?: " + (copiaNap == napuh));
-        System.out.println("equals?: " + napuh.equals(copiaNap));
-        System.out.println(napuh.toString());
-        System.out.println(copiaNap.toString());
 
     }
 
