fd4840b25cbf5277545c1ffb6d1c13a6d1be794a
Claudio Fernández <cfernf07@estudiantes.unileon.es>
2022-01-10 00:50:45 +0100

    Terminadas visualmente interfaces de aniadir y modifica prroducto

--- src/Vista/Producto/CRUDProductoVista.java
+++ src/Vista/Producto/CRUDProductoVista.java
@@ -6,10 +6,12 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -45,7 +47,7 @@ public class CRUDProductoVista extends JDialog{
 
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setVisible(true);
-		setBounds(100, 100, 526, 482);
+		setBounds(100, 100, 720, 524);
 		setTitle("Aniadir Producto");
 		getContentPane().setLayout(null);
 
@@ -55,7 +57,7 @@ public class CRUDProductoVista extends JDialog{
 		getContentPane().add(lbID);
 
 		tfID = new JTextField();
-		tfID.setBounds(316, 181, 94, 38);
+		tfID.setBounds(316, 34, 378, 38);
 		getContentPane().add(tfID);
 		tfID.setBounds(316, 34, 131, 38);
 
@@ -65,7 +67,7 @@ public class CRUDProductoVista extends JDialog{
 		getContentPane().add(lbPrecio);
 
 		tfPrecio = new JTextField();
-		tfPrecio.setBounds(316, 230, 94, 38);
+		tfPrecio.setBounds(316, 83, 165, 38);
 		getContentPane().add(tfPrecio);
 		tfPrecio.setBounds(316, 83, 131, 38);
 
@@ -74,30 +76,37 @@ public class CRUDProductoVista extends JDialog{
 		lbImagen.setBounds(10, 137, 77, 33);
 		getContentPane().add(lbImagen);
 
-		tfImagen = new JTextField();
-		tfImagen.setColumns(10);
-		tfImagen.setBounds(316, 132, 131, 38);
-		getContentPane().add(tfImagen);
-
 		JLabel lbProveedor = new JLabel("Proveedor");
 		lbProveedor.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lbProveedor.setBounds(10, 186, 77, 33);
 		getContentPane().add(lbProveedor);
 
 		tfProveedor = new JTextField();
-		tfProveedor.setBounds(316, 181, 131, 38);
+		tfProveedor.setBounds(316, 181, 378, 38);
 		getContentPane().add(tfProveedor);
 		tfProveedor.setColumns(10);
 
         JLabel lbDescripcion = new JLabel("Descripcion");
 		lbDescripcion.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lbDescripcion.setBounds(10, 280, 107, 33);
+		lbDescripcion.setBounds(10, 328, 107, 33);
 		getContentPane().add(lbDescripcion);
 
 		tfDescripcion = new JTextField();
-		tfDescripcion.setBounds(316, 280, 131, 38);
+		tfDescripcion.setBounds(316, 328, 378, 93);
 		getContentPane().add(tfDescripcion);
 		tfDescripcion.setColumns(10);
+		
+		JButton btnSeleccionIm = new JButton("Selecciona una imagen");
+		btnSeleccionIm.setBounds(316, 132, 154, 38);
+		getContentPane().add(btnSeleccionIm);
+		btnSeleccionIm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser selectorImagen = new JFileChooser();
+				selectorImagen.showDialog(null, "Selecciona una imagen");
+				selectorImagen.setCurrentDirectory(new File("."));
+			}
+		});
+		
 
 		JLabel lbTipoProducto = new JLabel("Tipo de Producto");
 		lbTipoProducto.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -107,20 +116,21 @@ public class CRUDProductoVista extends JDialog{
 		JButton okButton = new JButton("Aniadir");
 		okButton.setActionCommand("OK");
 		getContentPane().add(okButton);
-        okButton.setBounds(280, 395, 100, 35);
+        okButton.setBounds(506, 454, 89, 23);
 		getRootPane().setDefaultButton(okButton);
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("Aniadir boton pulsado");
-				setVisible(false);
 				crearProducto();
 				limpiarCampos();
+				setVisible(false);
+				
 			}
 		});
 
 		JButton cancelButton = new JButton("Cancelar");
 		cancelButton.setActionCommand("Cancel");
-        cancelButton.setBounds(400, 395, 100, 35);
+        cancelButton.setBounds(605, 454, 89, 23);
 		getContentPane().add(cancelButton);
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -130,34 +140,50 @@ public class CRUDProductoVista extends JDialog{
 
 		comboBoxTipoProducto = new JComboBox();
 		comboBoxTipoProducto.setToolTipText("Selecciona\r\n");
-		comboBoxTipoProducto.setBounds(316, 230, 131, 38);
+		comboBoxTipoProducto.setBounds(316, 230, 378, 38);
 		getContentPane().add(comboBoxTipoProducto);
 		comboBoxTipoProducto.addItem("default");
-		comboBoxTipoProducto.addItem("Administrador");
-		comboBoxTipoProducto.addItem("Cajero");
-
+		comboBoxTipoProducto.addItem("Pendiente");
+		comboBoxTipoProducto.addItem("Anillo");
+		comboBoxTipoProducto.addItem("Pulsera");
+		comboBoxTipoProducto.addItem("Colgante");
+		comboBoxTipoProducto.addItem("Gafas");
+		
+		JLabel lbTipoMaterial = new JLabel("Tipo de Material");
+		lbTipoMaterial.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lbTipoMaterial.setBounds(10, 282, 200, 35);
+		getContentPane().add(lbTipoMaterial);
+		
+		JComboBox comboBoxTipoMaterial = new JComboBox();
+		comboBoxTipoMaterial.setToolTipText("Selecciona\r\n");
+		comboBoxTipoMaterial.setBounds(316, 279, 378, 38);
+		getContentPane().add(comboBoxTipoMaterial);
+		comboBoxTipoMaterial.addItem("default");
+		comboBoxTipoMaterial.addItem("Oro");
+		comboBoxTipoMaterial.addItem("Plata");
+		comboBoxTipoMaterial.addItem("Oro + Plata");
 	}
 	
 	public void pulsarBotonModificar(ProductoVO producto) {
 		
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setVisible(true);
-		setBounds(100, 100, 526, 482);
+		setBounds(100, 100, 720, 524);
 		setTitle("Modificar Producto");
 		getContentPane().setLayout(null);
-		
+
 		JTextField tfID;
 		JTextField tfPrecio;
-		JTextField tfImagen;
 		JTextField tfProveedor;
-		JTextField tfTipoProducto;
-        JLabel lbID = new JLabel("ID");
+		JTextField tfDescripcion;
+
+		JLabel lbID = new JLabel("ID");
 		lbID.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lbID.setBounds(10, 34, 113, 33);
 		getContentPane().add(lbID);
 
 		tfID = new JTextField();
-		tfID.setBounds(316, 181, 94, 38);
+		tfID.setBounds(316, 34, 378, 38);
 		getContentPane().add(tfID);
 		tfID.setBounds(316, 34, 131, 38);
 
@@ -167,7 +193,7 @@ public class CRUDProductoVista extends JDialog{
 		getContentPane().add(lbPrecio);
 
 		tfPrecio = new JTextField();
-		tfPrecio.setBounds(316, 230, 94, 38);
+		tfPrecio.setBounds(316, 83, 165, 38);
 		getContentPane().add(tfPrecio);
 		tfPrecio.setBounds(316, 83, 131, 38);
 
@@ -176,43 +202,67 @@ public class CRUDProductoVista extends JDialog{
 		lbImagen.setBounds(10, 137, 77, 33);
 		getContentPane().add(lbImagen);
 
-		tfImagen = new JTextField();
-		tfImagen.setColumns(10);
-		tfImagen.setBounds(316, 132, 131, 38);
-		getContentPane().add(tfImagen);
-
 		JLabel lbProveedor = new JLabel("Proveedor");
 		lbProveedor.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lbProveedor.setBounds(10, 186, 77, 33);
 		getContentPane().add(lbProveedor);
 
 		tfProveedor = new JTextField();
-		tfProveedor.setBounds(316, 181, 131, 38);
+		tfProveedor.setBounds(316, 181, 378, 38);
 		getContentPane().add(tfProveedor);
 		tfProveedor.setColumns(10);
 
         JLabel lbDescripcion = new JLabel("Descripcion");
 		lbDescripcion.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lbDescripcion.setBounds(10, 280, 107, 33);
+		lbDescripcion.setBounds(10, 328, 107, 33);
 		getContentPane().add(lbDescripcion);
 
 		tfDescripcion = new JTextField();
-		tfDescripcion.setBounds(316, 280, 131, 38);
+		tfDescripcion.setBounds(316, 328, 378, 93);
 		getContentPane().add(tfDescripcion);
 		tfDescripcion.setColumns(10);
+		
+		JButton btnSeleccionIm = new JButton("Selecciona una imagen");
+		btnSeleccionIm.setBounds(316, 132, 154, 38);
+		getContentPane().add(btnSeleccionIm);
+		btnSeleccionIm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser selectorImagen = new JFileChooser();
+				selectorImagen.showDialog(null, "Selecciona una imagen");
+				selectorImagen.setCurrentDirectory(new File("."));
+			}
+		});
+		
 
 		JLabel lbTipoProducto = new JLabel("Tipo de Producto");
 		lbTipoProducto.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lbTipoProducto.setBounds(10, 233, 200, 35);
 		getContentPane().add(lbTipoProducto);
 
-        comboBoxTipoProducto = new JComboBox();
+		comboBoxTipoProducto = new JComboBox();
 		comboBoxTipoProducto.setToolTipText("Selecciona\r\n");
-		comboBoxTipoProducto.setBounds(316, 230, 131, 38);
+		comboBoxTipoProducto.setBounds(316, 230, 378, 38);
 		getContentPane().add(comboBoxTipoProducto);
 		comboBoxTipoProducto.addItem("default");
-		comboBoxTipoProducto.addItem("Administrador");
-		comboBoxTipoProducto.addItem("Cajero");
+		comboBoxTipoProducto.addItem("Pendiente");
+		comboBoxTipoProducto.addItem("Anillo");
+		comboBoxTipoProducto.addItem("Pulsera");
+		comboBoxTipoProducto.addItem("Colgante");
+		comboBoxTipoProducto.addItem("Gafas");
+		
+		JLabel lbTipoMaterial = new JLabel("Tipo de Material");
+		lbTipoMaterial.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lbTipoMaterial.setBounds(10, 282, 200, 35);
+		getContentPane().add(lbTipoMaterial);
+		
+		JComboBox comboBoxTipoMaterial = new JComboBox();
+		comboBoxTipoMaterial.setToolTipText("Selecciona\r\n");
+		comboBoxTipoMaterial.setBounds(316, 279, 378, 38);
+		getContentPane().add(comboBoxTipoMaterial);
+		comboBoxTipoMaterial.addItem("default");
+		comboBoxTipoMaterial.addItem("Oro");
+		comboBoxTipoMaterial.addItem("Plata");
+		comboBoxTipoMaterial.addItem("Oro + Plata");
 
 		int ID = producto.getIDProducto();
 		double Precio = producto.getPrecio();
@@ -226,19 +276,20 @@ public class CRUDProductoVista extends JDialog{
 		JButton okButton = new JButton("Modificar");
 		okButton.setActionCommand("OK");
 		getContentPane().add(okButton);
-        okButton.setBounds(280, 395, 100, 35);
+        okButton.setBounds(506, 454, 89, 23);
 		getRootPane().setDefaultButton(okButton);
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				setVisible(false);
-				crearProducto();
+				System.out.println("Aniadir boton pulsado");
 				limpiarCampos();
+				setVisible(false);
+				
 			}
 		});
 
 		JButton cancelButton = new JButton("Cancelar");
 		cancelButton.setActionCommand("Cancel");
-        cancelButton.setBounds(400, 395, 100, 35);
+        cancelButton.setBounds(605, 454, 89, 23);
 		getContentPane().add(cancelButton);
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
